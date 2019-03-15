1bcb269b9f33c48c916f14f13d0fcf0a6dda0b37
jjuchoa <jbfdujunior@gmail.com>
2019-03-15 17:10:42 -0300

    Utilizando DjangoFilter backend

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -6,3 +6,5 @@ from atracoes.api.serializers import AtracaoSerializer
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filterset_fields = ('nome', 'descricao')
+
