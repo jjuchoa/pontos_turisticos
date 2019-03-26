c6e819afdb558f656482fa32516fa25b52c2e993
jjuchoa <jbfdujunior@gmail.com>
2019-03-26 14:00:14 -0300

    Habilitando o mecanismo de Tokens para autenticação

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -24,6 +24,8 @@ from atracoes.api.viewsets import AtracaoViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
 from enderecos.api.viewsets import EnderecoViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 admin.autodiscover()
 
@@ -38,4 +40,5 @@ router.register(r'enderecos', EnderecoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
